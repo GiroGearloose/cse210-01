1e2db6c9c16b5d86580f35f986a51ca718b69bf5
GiroGearloose <lop17023@byui.edu>
2022-04-30 22:30:39 -0300

    Diagram.py

--- Diagram.py
+++ Diagram.py
@@ -1,34 +1,58 @@
-# Author: Paulo Sergio Lopes
-# TIC-TAC-TOE game code.
-# Working on Github
+# Code of Tic Tac Toe game 
+# Version by Paulo Lopes
+# CSE 210 Brother Carter
 
-def game_screen():
-    
-   print("1|2|3") 
-   print("-+-+-")
-   print("4|5|6") 
-   print("-+-+-")
-   print("7|8|9") 
-   print("-+-+-")
+def main():
+    gamer = other_gamer("")
+    screen = create_screen()
+    while not (winner(screen) or my_print(screen)):
+        show_screen(screen)
+        make_move(gamer, screen)
+        gamer = other_gamer(gamer)
+    show_screen(screen)
+    print("Weel Done. Try again and Good Luck!") 
 
-def search_replace():
+def create_screen():
+    screen = []
+    for square in range(9):
+        screen.append(square + 1)
+    return screen
+
+def show_screen(screen):
+    print()
+    print(f"{screen[0]}|{screen[1]}|{screen[2]}")
+    print('-+-+-')
+    print(f"{screen[3]}|{screen[4]}|{screen[5]}")
+    print('-+-+-')
+    print(f"{screen[6]}|{screen[7]}|{screen[8]}")
     print()
     
-def play():
-    player = input("player 1 type X player 2 type O: ")
-    Choicest_number = input("Type your choicest number from 1 to 9: ")
-    if player.upper() == "X":
-     search_replace()
-    elif player.upper() == "O":
-     search_replace() 
-    else:
-     print("aqui")
-   
-print("You can play TiC-TaC-Toe")
-choice = input("to Play type Y and to quit type N: ")
+def my_print(screen):
+    for square in range(9):
+        if screen[square] != "x" and screen[square] != "o":
+            return False
+    return True 
+    
+def winner(screen):
+    return (screen[0] == screen[1] == screen[2] or
+            screen[3] == screen[4] == screen[5] or
+            screen[6] == screen[7] == screen[8] or
+            screen[0] == screen[3] == screen[6] or
+            screen[1] == screen[4] == screen[7] or       
+            screen[2] == screen[5] == screen[8] or
+            screen[0] == screen[4] == screen[8] or
+            screen[2] == screen[4] == screen[6])
+            
+            
+def make_move(gamer, screen):
+    square = int(input(f"{gamer} please your turn choose square (1-9): "))
+    screen[square - 1] = gamer
 
-if choice.upper() == "Y":  
-    game_screen()
-    play()
-else:
-    print("OK see you later")
+def other_gamer(actual):
+    if actual == "" or actual == "o":
+        return "x"
+    elif actual == "x":
+        return "o"
+
+if __name__ == "__main__":
+    main()
